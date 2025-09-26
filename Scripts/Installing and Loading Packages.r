11234bdf5fea8d4ee8fb86cc29f4e9355117113d
nigi1992 <127438655+nigi1992@users.noreply.github.com>
2025-09-26 10:12:00 +0200

    renaming and replacing

---
+++ Scripts/Installing and Loading Packages.r
@@ -0,0 +1,24 @@
+# install packages for course
+
+install.packages(c("tidyverse", "tidymodels"))
+
+install.packages(c("glmnet", "rpart", "randomForest"))
+
+install.packages(c("xgboost", "nnet"))
+
+install.packages(c("corrplot", "rpart.plot", "vip"))
+
+install.packages(c("doParallel", "tidyclust"))
+
+library(tidyverse)
+library(tidymodels)
+library(glmnet)
+library(rpart)
+library(randomForest)
+library(xgboost)
+library(nnet)
+library(corrplot)
+library(rpart.plot)
+library(vip)
+library(doParallel)
+library(tidyclust)
